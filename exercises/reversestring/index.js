380dd43027429b8104c1cfa879031229f0c1184f
Edxael <zerphyr.r@gmail.com>
2017-12-27 16:19:28 -0700

    add the For ES5

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,4 +10,31 @@ function reverse(str) {
   return str.split("").reverse().join("")
 }
 
+
+
+  // Using a For loop
+function reverse(str) {
+
+  const arr = []
+  for(let x = str.length; x >= 0; x--){
+    arr.push(str[x])
+  }
+
+  return arr.join("")
+}
+
+
+// Using the ES5 For Loops
+function reverse(str) {
+
+  let reversed = ''
+  for(let character of str){
+    reversed = character + reversed
+  }
+
+  return reversed
+
+}
+
+
 module.exports = reverse;
